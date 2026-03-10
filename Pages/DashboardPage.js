289b66e8ba4f13c07f9afc77f6eee7109a3d0ffa
SonyPlayJunior <sonyplayjunior@gmail.com>
2026-03-10 11:36:14 -0500

    prueba codigo de dasboar

--- Pages/DashboardPage.js
+++ Pages/DashboardPage.js
@@ -1,3 +1,10 @@
 export class DashboardPage{
-    
+    constructor(page){
+        this.page = page
+        this.menuInicio = page.getBytext('Inicio')
+    }
+
+    async isDashboardVisible(){
+        return await this.menuInicio.isVisible()
+    }
 }
--- tests/test/Login.spec.js
+++ tests/test/Login.spec.js
@@ -1,17 +1,23 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../Pages/LoginPage';
+import { DashboardPage } from '../../Pages/DashboardPage';
+
 
 test('test_login', async ({ page }) => {
 
   await page.goto('https://iam-test.titularizadora.com/realms/titularizadora/protocol/openid-connect/auth?client_id=archivos-client&redirect_uri=https%3A%2F%2Fcarchivos-web-dev.titularizadora.com%2Fdashboard%2Findex&response_type=code&scope=openid&response_mode=query');
 
   const loginPage = new LoginPage(page);
+  const dashboardPage = new DashboardPage(page)
 
   await loginPage.login(
     'wcontreras@titularizadora.com',
     'wcontreras'
   );
 
-await expect(page).toHaveURL('https://carchivos-web-dev.titularizadora.com/dashboard/index');
-await expect(page.getByText('Configuración')).toBeVisible();
+//await expect(page).toHaveURL('https://carchivos-web-dev.titularizadora.com/dashboard/index');
+//await expect(page.getByText('Configuración')).toBeVisible();
+expect(await DashboardPage.isDashboardPageVisible()).toBe(true)
+
+
 });
