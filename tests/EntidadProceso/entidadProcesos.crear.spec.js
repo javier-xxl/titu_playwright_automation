d98bd9383e780831f290dcace3412f6bd3db37b1
Javier XXL <sonyplauyjunior@gmail.com>
2026-03-19 11:40:51 -0500

    h

--- tests/EntidadProceso/entidadProcesos.crear.spec.js
+++ tests/EntidadProceso/entidadProcesos.crear.spec.js
@@ -1,24 +1,32 @@
-import{test,expect} from '@playwright/test'
-import { EntidadProcesoPage } from '../../Pages/EntidadProceso'
-import { ConfiguracionPage } from '../../Pages/ConfiguracionPage'   
+import { test, expect } from "@playwright/test";
+import { EntidadProcesoPage } from "../../Pages/EntidadProceso";
+import { ConfiguracionPage } from "../../Pages/ConfiguracionPage";
 
-test('Crear entidad proceso',async({page})=>{
+test("Crear entidad proceso", async ({ page }) => {
+  await page.goto(
+    "https://carchivos-web-dev.titularizadora.com/dashboard/index",
+  );
+  const configuracionPage = new ConfiguracionPage(page);
+  const entidadProcesoPage = new EntidadProcesoPage(page);
+  const inputCorreos = page.locator("textarea").first();
 
-    await page.goto('https://carchivos-web-dev.titularizadora.com/dashboard/index');
-    const configuracionPage = new ConfiguracionPage(page);
-    const entidadProcesoPage = new EntidadProcesoPage(page);
+  await configuracionPage.menuConfiguracion.click();
+  await page.getByText("Entidad procesos").click();
 
-    await configuracionPage.menuConfiguracion.click();
-    await page.getByText('Entidad procesos').click();
+  await expect(page).toHaveURL(/entity-process/);
+  await entidadProcesoPage.ValidarCargue();
 
-    await expect(page).toHaveURL(/entity-process/);
-    await entidadProcesoPage.ValidarCargue();
+  await entidadProcesoPage.irACrear();
 
-    await entidadProcesoPage.irACrear();
+  await expect(inputCorreos).toHaveValue("test@correo.com,test2@correo.com");
 
-    await entidadProcesoPage.seleccionarEntidad('MTI S.A - Thomas');
+  await entidadProcesoPage.seleccionarEntidad("MTI S.A - Thomas");
 
-    await entidadProcesoPage.seleccionarTodoslosprocesos();
+  await entidadProcesoPage.seleccionarTodoslosprocesos();
 
-    await entidadProcesoPage.guardar();
-})
+  await entidadProcesoPage.guardar();
+
+  await expect(page.getByRole("alert")).toContainText(
+    "Se han asociados los procesos a la entidad exitosamente.",
+  );
+});
